b37383a23cbda36006b172a7aab65e5953cd6b08
Lukman Sk <skd1746289@gmail.com>
2023-10-23 09:43:51 +0530

    feat(server): set up routes for getPost and createPost

--- v1/server/routes/posts.js
+++ v1/server/routes/posts.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
-const { getPosts } = require("../controllers/posts");
+const { getPosts, createPost } = require("../controllers/posts");
 
 const router = express.Router();
 
 // http://localhost:5000/posts
 
 router.get("/", getPosts);
+router.post("/", createPost);
 
 module.exports = router;
