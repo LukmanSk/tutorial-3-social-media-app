16a4ac2c800e33d561d445a89488efaed024cc52
Lukman Sk <skd1746289@gmail.com>
2023-10-24 05:21:47 +0530

    feat(client): integrate Redux for state management

--- v1/client/src/App.jsx
+++ v1/client/src/App.jsx
@@ -1,5 +1,46 @@
+import { Container, Typography, AppBar, Grow, Grid } from "@material-ui/core";
+import memories from "./images/memories.png";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import useStyles from "./styles.js";
+import { useSelector } from "react-redux";
+
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+  console.log(posts);
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height={60}
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
--- v1/client/src/main.jsx
+++ v1/client/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
