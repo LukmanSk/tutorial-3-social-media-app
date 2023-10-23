066ceffe84ee32fbad8f9745a642a40f364c4bd8
Lukman Sk <skd1746289@gmail.com>
2023-10-23 07:40:45 +0530

    feat(server): connect database to backend

--- v1/server/index.js
+++ v1/server/index.js
@@ -2,3 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = `mongodb+srv://bsl_1746:bsl_1746@cluster0.lnuerzl.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server Running on : ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
