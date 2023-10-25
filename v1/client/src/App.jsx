996575866c622f3b7d3efb22f398d7923c4746e4
Lukman Sk <skd1746289@gmail.com>
2023-10-25 18:04:36 +0530

    feat(client): implement Form component using Material UI

--- v1/client/src/App.jsx
+++ v1/client/src/App.jsx
@@ -3,7 +3,7 @@ import memories from "./images/memories.png";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
 import useStyles from "./styles.js";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getPosts } from "./actions/posts";
 
