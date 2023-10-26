0120852b1fe5a8b46c88246459bac7d7edf848f9
Lukman Sk <skd1746289@gmail.com>
2023-10-26 07:51:53 +0530

    style(client): implement Material UI styles in Form component

--- v2/client/src/App.jsx
+++ v2/client/src/App.jsx
@@ -6,9 +6,11 @@ import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
 import { useEffect } from "react";
 import { getPosts } from "./actions/posts";
+import useStyles from "./styles";
 const App = () => {
   const posts = useSelector((state) => state.posts);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getPosts);
@@ -18,11 +20,17 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" height={60} width={60} />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="icon"
+          height="60"
+          width="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
