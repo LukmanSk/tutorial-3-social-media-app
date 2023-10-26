a8852272c16a2e304b1dd37f32b78381ce1d3703
Lukman Sk <skd1746289@gmail.com>
2023-10-26 06:53:35 +0530

    feat(client): implement Redux reducers and actions for fetchPosts

---
+++ v2/client/src/App.jsx
@@ -0,0 +1,48 @@
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import memories from "./images/memories.png";
+
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import { useEffect } from "react";
+import { getPosts } from "./actions/posts";
+const App = () => {
+  const posts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts);
+  }, [dispatch]);
+
+  console.log(posts);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height={60} width={60} />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
